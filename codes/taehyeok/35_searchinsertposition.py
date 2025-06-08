a6692f12840953149327e7662c231edaa8e225a2
taehyeok <ritmakid@gmail.com>
2025-06-08 16:22:12 +0900

    week16 - 35 serach insert position

---
+++ codes/taehyeok/35_searchinsertposition.py
@@ -0,0 +1,16 @@
+from collections import List
+
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l = 0
+        r = len(nums) - 1
+        while l <= r:
+            m = (l + r + 1) // 2
+            if target > nums[m]:
+                l = m + 1
+            elif target < nums[m]:
+                r = m - 1
+            else:
+                return m
+
+        return l
