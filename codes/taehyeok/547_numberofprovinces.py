e33840d0db42a657df302548b0793c66e595024c
이태혁(Lee TaeHyeok) <ritmakid@gmail.com>
2025-08-05 11:48:02 +0900

    이태혁(Lee TaeHyeok) - week20 (547.Number_of_Provinces)

---
+++ codes/taehyeok/547_numberofprovinces.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class DSU:
+    def __init__(self, capacity):
+        self.parent = [i for i in range(0, capacity)]
+
+    def find(self, x):
+        if x == self.parent[x]:
+            return self.parent[x]
+        return self.find(self.parent[x])
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        if root_x == root_y:
+            return
+        self.parent[root_x] = root_y
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        dsu = DSU(n)
+        for row in range(len(isConnected)):
+            for col in range(len(isConnected[0])):
+                if isConnected[row][col] == 1:
+                    dsu.union(row, col)
+        province_root = set()
+        for node in dsu.parent:
+            parent = dsu.find(node)
+            province_root.add(parent)
+        return len(province_root)
